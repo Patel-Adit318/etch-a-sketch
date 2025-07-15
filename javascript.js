71896bd01bb7528cbf23101d58ead4f60f055ae6
Adit Patel <aditkpatel318@gmail.com>
2025-07-15 10:32:44 +0530

    Added functionality to change grid background on hover

--- javascript.js
+++ javascript.js
@@ -2,5 +2,8 @@ const container = document.querySelector(".container");
 
 for(let i=1;i<=16*16;i++) {
     const div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor="black";
+    })
     container.appendChild(div);
 }
